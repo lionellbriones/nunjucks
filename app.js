4c7e63b3ea56cc15438382a9c5eae6b6c43d1316
DIR\lionell.c.briones <lionell.c.briones@accenture.com>
2017-10-02 10:27:40 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const app = express();
+
+app.set('view engine', 'njk');
+
+app.use('/static-files', express.static('static'));
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/template', (req, res, next) => {
+    res.render('child');
+})
+
+app.use((req, res, next) => {
+    var items = [{ title: "foo", id: 1 }, { title: "bar", id: 2}];
+    var points = [[0, 1, 2], [5, 6, 7], [12, 13, 14]];
+    
+    res.render('index', {
+        title: 'NunJucks',
+        sampleList: [1, 2, 3, 4],
+        items: items,
+        points: points
+    });
+});
+
+app.listen(3000);
