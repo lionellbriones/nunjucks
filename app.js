99984807f6aad633b91bf4ff50700b17483b7ee9
DIR\lionell.c.briones <lionell.c.briones@accenture.com>
2017-10-02 13:55:08 +0800

    More features

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ app.get('/template', (req, res, next) => {
         title: 'NunJucks',
         items: items
     });
-})
+});
+
+app.get('/import', (req, res, next) => {
+    var items = [{ name: "foo", id: 1 }, { name: "bar", id: 2}];
+    res.render('import', {
+        title: 'NunJucks',
+        items: items
+    });
+});
 
 app.use((req, res, next) => {
     var items = [{ title: "foo", id: 1 }, { title: "bar", id: 2}];
